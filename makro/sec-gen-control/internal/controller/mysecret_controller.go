64af49ec7566f2f36a6978fff8513ae776534afb
chin008 <93816719+chin008@users.noreply.github.com>
2025-05-16 00:02:44 +0700

    Update mysecret_controller.go

--- makro/sec-gen-control/internal/controller/mysecret_controller.go
+++ makro/sec-gen-control/internal/controller/mysecret_controller.go
@@ -47,14 +47,6 @@ type MySecretReconciler struct {
 // +kubebuilder:rbac:groups=core,resources=secrets/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the MySecret object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.20.4/pkg/reconcile
 func (r *MySecretReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
